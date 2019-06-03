89fd73790cf9df9c8a936d28a19784e25c436000
Ksenia Mizinova <ksenya-mizinova@yandex.ru>
2019-06-03 22:29:07 +0300

    DM config

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+xrandr --output $(xrandr | grep 'VGA[0-9] connected' | awk '{print $1}') --mode 1920x1200 --left-of $(xrandr | grep 'LVDS[0-9] connected' | awk '{print $1}')
+setxkbmap -option grp:ctrl_shift_toggle us,ru
+xmodmap .xmodmap_das
+amixer -D pulse set Master 1+ 100% on
+[ -z "`amixer get Capture | grep "\[on\]"`" ] && amixer set Capture 1+ 100% toggle
+pkill xscreensaver
