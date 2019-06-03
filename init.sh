2d5a4c3c8e034eebbccdf04a127736b23d1a9840
Ksenia Mizinova <ksenia.mizinova@gmail.com>
2019-06-04 00:23:35 +0300

    rc.lua: wallpaper does not ignore aspect ratio; spawn nm-applet async; tiling windows by default

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 xrandr --output $(xrandr | grep 'VGA[0-9] connected' | awk '{print $1}') --mode 1920x1200 --left-of $(xrandr | grep 'LVDS[0-9] connected' | awk '{print $1}')
 setxkbmap -option grp:ctrl_shift_toggle us,ru
 xmodmap .xmodmap_das
-amixer -D pulse set Master 1+ 100% on
+amixer -D pulse set Master 1+ 50% on
 [ -z "`amixer get Capture | grep "\[on\]"`" ] && amixer set Capture 1+ 100% toggle
 pkill xscreensaver
